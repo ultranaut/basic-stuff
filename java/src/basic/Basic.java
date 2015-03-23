1d2123c6a79b4160483134d2be653a42b38a39c9
John Shearer <john@ultranaut.com>
2015-03-23 12:47:29 -0400

    Make a title utility and use it

--- java/src/basic/Basic.java
+++ java/src/basic/Basic.java
@@ -1,33 +1,40 @@
 package basic;
 
+import utils.Titler;
+
 public class Basic {
   public static void main(String[] args) {
+    
+    Titler titler = new Titler(30);
 
+    // -------------------------------------------------------------------
     // hello world
-    System.out.println("Obligatory 'Hello, world!'");
+    System.out.println();
+
+    titler.display("Obligatory 'Hello, world!'");
+
     HelloWorld hello = new HelloWorld();
     hello.sayIt();
 
-    // -------------------------------------------------------------------
-    System.out.print(String.format("\n\n%60s\n\n", "").replace(' ', '-'));
 
-    System.out.println("Factorial");
+    // -------------------------------------------------------------------
+    // Factorials
+    titler.display("Factorial");
     Factorial fact = new Factorial();
 
     for (int i = 0; i < 20; i++) {
       System.out.printf("%2s! = %s\n", i, fact.factorialize(i));
     }
-    
+
 
     // -------------------------------------------------------------------
-    System.out.print(String.format("\n\n%60s\n\n", "").replace(' ', '-'));
-
-    System.out.println("Merge sort");
+    // Merge sort
+    titler.display("Merge sort");
     int[] list = {5, 2, 4, 3, 1};
     SortableList sortList = new SortableList(list);
-    
+
     int[] sorted = sortList.sort();
-    
+
     System.out.println(sortList.toString());
 
   }
---
+++ java/src/utils/Titler.java
@@ -0,0 +1,40 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.Formatter;
+
+public class Titler {
+  int width;
+
+  public Titler() {
+    this.width = 80;
+  }
+  
+  public Titler(int width) {
+    this.width = width;
+  }
+  
+  private String rule() {
+    Formatter str = new Formatter();
+    String rule;
+    
+    // gotta be a simpler way...
+    char[] chars = new char[this.width];
+    Arrays.fill(chars, '-');
+   
+    str.format("%s", new String(chars));
+    
+    rule = str.toString();
+    str.close();
+    
+    return rule;
+  }
+  
+  public void display(String heading) {
+    Formatter str = new Formatter();
+    
+    str.format("\n\n%s\n%s", heading, this.rule());
+    System.out.println(str.toString());
+    str.close();
+  }
+}
