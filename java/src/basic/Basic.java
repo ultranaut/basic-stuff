fefd7e524539e2bda7c10f183a501e475b7c2813
John Shearer <john@ultranaut.com>
2015-03-23 12:13:41 -0400

    Add some labels to the sections

--- java/src/basic/Basic.java
+++ java/src/basic/Basic.java
@@ -4,20 +4,25 @@ public class Basic {
   public static void main(String[] args) {
 
     // hello world
+    System.out.println("Obligatory 'Hello, world!'");
     HelloWorld hello = new HelloWorld();
     hello.sayIt();
 
     // -------------------------------------------------------------------
     System.out.print(String.format("\n\n%60s\n\n", "").replace(' ', '-'));
 
+    System.out.println("Factorial");
     Factorial fact = new Factorial();
 
-    long factResult = fact.factorialize(5);
-    System.out.println(factResult);
+    for (int i = 0; i < 20; i++) {
+      System.out.printf("%2s! = %s\n", i, fact.factorialize(i));
+    }
+    
 
     // -------------------------------------------------------------------
     System.out.print(String.format("\n\n%60s\n\n", "").replace(' ', '-'));
 
+    System.out.println("Merge sort");
     int[] list = {5, 2, 4, 3, 1};
     SortableList sortList = new SortableList(list);
     
