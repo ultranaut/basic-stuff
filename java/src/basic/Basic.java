b51678eee54acb62338ffb318fc3324bf9f7720d
John Shearer <john@ultranaut.com>
2015-03-23 11:16:11 -0400

    Get MergeSort working

--- java/src/basic/Basic.java
+++ java/src/basic/Basic.java
@@ -7,12 +7,27 @@ public class Basic {
     HelloWorld hello = new HelloWorld();
     hello.sayIt();
 
+    // -------------------------------------------------------------------
     System.out.print(String.format("\n\n%60s\n\n", "").replace(' ', '-'));
 
     Factorial fact = new Factorial();
-    
+
     long factResult = fact.factorialize(5);
     System.out.println(factResult);
+
+    // -------------------------------------------------------------------
+    System.out.print(String.format("\n\n%60s\n\n", "").replace(' ', '-'));
+
+    int[] sortList = {5, 2, 4, 3, 1};
+    MergeSort sort = new MergeSort(sortList);
+    int[] sorted = sort.sort();
+
+    StringBuilder sb = new StringBuilder();
+    sb.append("sorted list: ");
+    for (int item : sorted) {
+      sb.append(" " + item);
+    }
+    System.out.println(sb.toString());
 
   }
 }
--- java/src/basic/MergeSort.java
+++ java/src/basic/MergeSort.java
@@ -3,26 +3,76 @@ package basic;
 import java.util.Formatter;
 
 public class MergeSort {
-  private String[] list;
+  private int[] list;
   
-  public MergeSort(String[] list) {
+  public MergeSort(int[] list) {
     this.list = list;
   }
   
-  private int[] splitList(String[] list) {
-    int firstPart = list.length / 2;
-    System.out.println(firstPart);
-    int secondPart = list.length - firstPart;
-    int[] split = {firstPart, secondPart};
-    return split;
+  public int[] sort() {
+    return this.sort(list);
   }
   
-  public String toString() {
-    int[] split = splitList(list);
-    Formatter fmt = new Formatter();
-    fmt.format("(%d, %d)", split[0], split[1]);
+  public int[] sort(int[] list) {
+    if (list.length < 2) {
+      return list;
+    } else {
+      int[][] subs = splitList(list);
+      return merge(sort(subs[0]), sort(subs[1]));
+    }
+  }
+  
+  private int[] merge(int[] left, int[] right) {
+    int[] merged = new int[left.length + right.length];
     
-    return fmt.toString();
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    
+    while (true) {
+      if (left[i] < right[j]) {
+        merged[k] = left[i];
+        i++;
+      } else {
+        merged[k] = right[j];
+        j++;
+      }
+      k++;
+      if (i == left.length || j == right.length) {
+        break;
+      }
+    }
+    
+    while (i < left.length) {
+      merged[k] = left[i];
+      i++;
+      k++;
+    }
+    
+    while (j < right.length) {
+      merged[k] = right[j];
+      j++;
+      k++;
+    }
+    
+    return merged;
+  }
+  
+  // pretty sure this is probably *not* the way to do it
+  private int[][] splitList(int[] list) {
+    // figure out the lengths of the two split arrays
+    int lenSubA = list.length / 2;
+    int lenSubB = list.length - lenSubA;
+    
+    int[] subA = new int[lenSubA];
+    int[] subB = new int[lenSubB];
+
+    System.arraycopy(list, 0, subA, 0, lenSubA);
+    System.arraycopy(list, lenSubA, subB, 0, lenSubB);
+    
+    
+    int[][] split = {subA, subB};
+    return split;
   }
   
 }
